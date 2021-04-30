f4a81d8c126f299fdfe3c705f617287fae82dabc
Victor Caceres <caceres.vc95@gmail.com>
2021-04-30 00:28:32 -0400

    create atlantis game with heroku-nginx configuration

---
+++ static-html/src/Manager/init.js
@@ -0,0 +1,5 @@
+function scaleScenary(game) {
+  game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  game.scale.pageAlignHorizontally = true;
+  game.scale.pageAlignVertically = true;
+}
---
+++ static-html/src/Manager/preload.js
@@ -0,0 +1,11 @@
+function loadResource(game) {
+  game.load.image('background', 'assets/images/background.png');
+  game.load.spritesheet('horse', 'assets/images/horse.png', 84, 156, 2);
+  game.load.spritesheet('diamonds', 'assets/images/diamonds.png', 81, 84, 4);
+  game.load.image('explosion', 'assets/images/explosion.png');
+  game.load.image('shark', 'assets/images/shark.png');
+  game.load.image('fishes', 'assets/images/fishes.png');
+  game.load.image('mollusk', 'assets/images/mollusk.png');
+  game.load.image('booble1', 'assets/images/booble1.png');
+  game.load.image('booble2', 'assets/images/booble2.png');
+}
